19258b2334e3d5f0103f135257b5eeaf6a25c2cc
CEnciso1 <christophera.enciso@gmail.com>
2024-01-31 12:14:18 -0600

    Update Access-Control-Allow-Origin header

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      "Access-Control-Allow-Origin": "https://mylink-frontend.onrender.com", // Set to a specific origin or '*' for any origin
+    },
+  },
 });

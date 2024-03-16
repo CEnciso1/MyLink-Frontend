f6b20b8f619db9b0e8cb5ff2434cfa9c11803bb7
CEnciso1 <christophera.enciso@gmail.com>
2024-03-15 19:48:00 -0500

    access token call

--- src/components/InstagramAuth.tsx
+++ src/components/InstagramAuth.tsx
@@ -12,11 +12,11 @@ function InstagramAuth() {
     if (searchParams.get("code")) {
       const getToken = async () => {
         const data = {
-          client_id: process.env.REACT_APP_INSTAGRAM_APP_IDD,
-          client_secret: process.env.REACT_APP_INSTAGRAM_SECRET,
+          client_id: import.meta.env.VITE_APP_INSTAGRAM_APP_IDD,
+          client_secret: import.meta.env.VITE_APP_INSTAGRAM_SECRET,
           code: searchParams.get("code"),
           grant_type: "authorization_code",
-          redirect_uri: process.env.REACT_APP_INSTAGRAM_REDIRECT_URI,
+          redirect_uri: import.meta.env.VITE_APP_INSTAGRAM_REDIRECT_URI,
         };
         const requestBody = querystring.stringify(data);
         const response = await axios.post(
@@ -32,9 +32,52 @@ function InstagramAuth() {
       };
       getToken();
     }
-  });
+  }, []);
 
   return <div>test</div>;
 }
+
+// const getLongAccessToken = async () =>{
+//   try{
+
+//     const data = {
+//       client_id: import.meta.env.VITE_APP_INSTAGRAM_APP_IDD,
+//       client_secret: import.meta.env.VITE_APP_INSTAGRAM_SECRET,
+//       code: searchParams.get("code"),
+//       grant_type: "authorization_code",
+//       redirect_uri: import.meta.env.VITE_APP_INSTAGRAM_REDIRECT_URI,
+//     };
+//     const requestBody = querystring.stringify(data);
+//     const response = await axios.post(
+//       "https://api.instagram.com/oauth/access_token",
+//       requestBody,
+//       {
+//         headers: {
+//           "Content-Type": "application/x-www-form-urlencoded",
+//         },
+//       }
+//     );
+
+//   }catch(error){
+//     console.log(error)
+//   }
+// }
+
+const getMedia = async () => {
+  try {
+    const response = await axios.get(
+      "https://graph.instagram.com/instagram-api",
+      {
+        params: {
+          client_id: import.meta.env.VITE_APP_INSTAGRAM_APP_IDD,
+          redirect_uri: import.meta.env.VITE_APP_INSTAGRAM_REDIRECT_URI,
+          scope: "user_profile,user_media",
+          response_type: "code",
+        },
+        withCredentials: true,
+      }
+    );
+  } catch (error) {}
+};
 
 export default InstagramAuth;
